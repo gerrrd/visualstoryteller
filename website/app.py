6da538744e407b0dcfb115a512af45615bf42c68
Erika Grosse Hokamp <erika.grossehokamp@gmail.com>
2021-03-03 17:34:06 +0100

    updated file. Contains now a working version of our platform :-)

--- website/app.py
+++ website/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import time
-
+from visualstoryteller.getonepic import getonepic
+import matplotlib.pyplot as plt
+import nltk
+nltk.download('word2vec_sample')
+nltk.download('averaged_perceptron_tagger')
 
 st.markdown('''
 
@@ -14,14 +18,26 @@ I am an artifical intelligence that turns your text into an image.
 text = st.text_input('Tell me something interesting:')
 
 if st.button('Submit'):
-    with st.spinner('Wait for it...'):
-        time.sleep(5)
-    st.success('Done!')
+    with st.spinner('I am generating your image...'):
+        time.sleep(30)
+    st.success('This is what I came up with:')
 
-    st.image(
-     "https://images.unsplash.com/photo-1537204696486-967f1b7198c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
-    )
+    result = getonepic(text, show_result=True)
 
+    fig, ax = plt.subplots()
+    im = ax.imshow(result['image'][0])
+    plt.axis('off')
+    st.pyplot(fig)
+
+    content_author = result['content'][1]
+    content_profile = result['content'][2]
+    style_author = result['style'][1]
+    style_profile = result['style'][2]
+
+    attribution = f"Photos by [{content_author}]({content_profile}) and \
+        [{style_author}]({style_profile})"
+
+    st.markdown(attribution)
 
 st.markdown('''
 
