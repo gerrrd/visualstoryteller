48856670c3d4f3a37ed117df0b75b43ae736572c
Erika Grosse Hokamp <erika.grossehokamp@gmail.com>
2021-03-05 14:55:39 +0100

    creating a second api and interface for testing getmorepics_twonouns function

---
+++ api/fast_2.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from visualstoryteller.getonepic import getonepic
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+@app.get("/")
+def index():
+    return {"ok": "True"}
+
+
+@app.get("/image")
+def get_image(text):
+
+    result = getonepic(text, saveimage=True)
+
+    # image = result['image']
+    # content_image = result['content'][0]
+    # content_author = result['content'][1]
+    # content_profile = result['content'][2]
+    # style_image = result['style'][0]
+    # style_author = result['style'][1]
+    # style_profile = result['style'][2]
+
+    # pipeline = get_model_from_gcp()
+    #pipeline = joblib.load('model.joblib')
+
+    return result
---
+++ website/app_2.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import time
+import matplotlib.pyplot as plt
+import requests
+from PIL import Image
+
+url = 'http://localhost:8000/image'
+
+st.markdown('''
+
+# Visual Storyteller
+
+Hey there!
+I am an artifical intelligence that turns your text into an image.
+
+''')
+
+text = st.text_input('Tell me something interesting:')
+
+if st.button('Submit'):
+    with st.spinner('I am generating your image...'):
+        time.sleep(1)
+    st.success('This is what I came up with:')
+
+    params = {'text': text}
+
+    result = requests.get(url, params=params).json()
+
+    print(result)
+    jpeg = result['saved']
+    image = Image.open(jpeg)
+    st.image(image)
+
+    #fig, ax = plt.subplots()
+    #im = ax.imshow(result['image'])
+    #plt.axis('off')
+    #st.pyplot(fig)
+
+    content_image = result['content'][0]
+    content_author = result['content'][1]
+    content_profile = result['content'][2]
+    style_image = result['style'][0]
+    style_author = result['style'][1]
+    style_profile = result['style'][2]
+
+    attribution = f"Photos by [{content_author}]({content_profile}) and \
+        [{style_author}]({style_profile})"
+
+    images_links = f"[Image 1]({content_image}) / [Image 2]({style_image})"
+
+    st.markdown(attribution)
+    st.markdown(images_links)
+
+st.markdown('''
+
+Images from [Unsplash](https://unsplash.com/)
+
+''')
+
+
