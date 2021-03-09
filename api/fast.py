058b0ccebe0344f79624df434e3a6d3e300958dd
Erika Grosse Hokamp <erika.grossehokamp@gmail.com>
2021-03-09 11:54:23 +0100

    working on api and website to include final getmorepics function

--- api/fast.py
+++ api/fast.py
@@ -22,29 +22,31 @@ def index():
 @app.get("/image")
 def get_image(text):
 
+    # get the images
     result = getonepic(text, saveimage=True)
 
+    # check if the response is empty
+    if result['OK'] == 0:
+        return result
+
+    # upload the pictures to google storage
     bucket_name = 'lewagon-bootcamp-305809'
     storage_location = 'project'
-    path_to_file = 'output.jpg'
-
     client = storage.Client()
     bucket = client.bucket(bucket_name)
-    blob = bucket.blob(f'{storage_location}/{path_to_file}')
-    blob.upload_from_filename(path_to_file)
-    blob.make_public()
-    im_url = blob.public_url
+    im_urls = []
 
-    result['im_url'] = im_url
-    # image = result['image']
-    # content_image = result['content'][0]
-    # content_author = result['content'][1]
-    # content_profile = result['content'][2]
-    # style_image = result['style'][0]
-    # style_author = result['style'][1]
-    # style_profile = result['style'][2]
+    for img in result['saved']:
 
-    # pipeline = get_model_from_gcp()
-    #pipeline = joblib.load('model.joblib')
+        path_to_file = img
+
+        blob = bucket.blob(f'{storage_location}/{path_to_file}')
+        blob.upload_from_filename(path_to_file)
+        blob.make_public()
+        im_url = blob.public_url
+
+        im_urls.append(im_url)
+
+    result['im_urls'] = im_urls
 
     return result
